6e167f6b0abf44d27ce424e81baf3572531b6d06
Alifur Rahman <alifurrahman1999@gmail.com>
2023-08-16 18:10:41 +0600

    Store card item in local stroage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Products/Product';
 import './Shop.css';
@@ -12,10 +13,37 @@ const Shop = () => {
         .then(data => setProducts(data));
     },[])
 
-    const HandelAddtoCart = (product) =>{
-        const newCart = [...cart,product];
+    const HandelAddtoCart = (seletedProduct) =>{
+        let newCart = [];
+        const exist = cart.find(product => product.id === seletedProduct.id);
+        if(!exist){
+            seletedProduct.quantity = 1;
+            newCart = [...cart,seletedProduct];
+        }else{
+            const rest = cart.filter(product => product.id !== seletedProduct.id);
+            exist.quantity = exist.quantity+1;
+            newCart = [...rest,exist];
+        }
+
+        // const newCart = [...cart,seletedProduct];
         setCart(newCart);
+        addToDb(seletedProduct.id);
     }
+
+    useEffect(()=>{
+       const strodCard = getShoppingCart();
+       const savedCard = [];
+        for(const id in strodCard){
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                addedProduct.quantity = strodCard[id];
+                savedCard.push(addedProduct);
+            }
+        }
+
+        setCart(savedCard);
+        
+    },[products])
 
     return (
         <div className='shoping_container'>
